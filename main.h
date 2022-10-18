19404b29f58f80d61ef6600fae7cdb15dc41f9cd
Thatcher Chen <thatcherchen@163.com>
2022-10-19 00:02:12 +0800

    Complete cursor abstraction.

--- main.h
+++ main.h
@@ -70,6 +70,12 @@ typedef struct {
     Pager *pager;
 } Table;
 
+typedef struct {
+    Table *table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
 
 extern const uint32_t ID_SIZE;
 extern const uint32_t USERNAME_SIZE;
@@ -93,7 +99,6 @@ PrepareResult prepare_insert(InputBuffer *inputBuffer, Statement *statement);
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
 void serialize_row(Row *source, void *destination);
 void deserialize_row(void *source, Row *destination);
-void *row_slot(Table *table, uint32_t row_num);
 ExecuteResult execute_insert(Statement *statement, Table *table);
 ExecuteResult execute_select(Statement *statement, Table *table);
 ExecuteResult execute_statement(Statement *statement, Table *table);
@@ -102,5 +107,9 @@ Table *db_open(const char *filename);
 void db_close(Table *table);
 void *get_page(Pager *pager, uint32_t page_num);
 void pager_flush(Pager *pager, uint32_t page_num, uint32_t size);
+Cursor *table_start(Table *table);
+Cursor *table_end(Table *table);
+void cursor_advance(Cursor *cursor);
+void *cursor_value(Cursor *cursor);
 
 #endif
